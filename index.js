d55a3f7a0263ba6b6400a9bb0551abb5ea144c4c
Imantha7 <imanthapiyumal7@gmail.com>
2025-01-25 21:59:41 +0530

    implement the dotnet in the project 👌

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
 import productRouter from "./routes/productRouter.js";
 import jwt, { decode } from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config(); 
 
 const app = express();
 
@@ -27,7 +30,7 @@ app.use((req,res,next)=>{
         next();
 })
 
-let mongoUrl = "mongodb+srv://kpiyumal:kpiyumal9@cluster0.rxrtgnt.mongodb.net/prods?retryWrites=true&w=majority&appName=Cluster0"
+let mongoUrl = process.env.MONGO_URL;
 
 mongoose.connect(mongoUrl)
 
